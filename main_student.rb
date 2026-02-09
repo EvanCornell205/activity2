6928117e1157f760cb25046fe6a29eac71a95146
Evan Cornell <evan.m.cornell@gmail.com>
2026-02-09 15:29:21 -0500

    student main

---
+++ main_student.rb
@@ -0,0 +1,18 @@
+require_relative 'student'
+
+# Create a Student instance
+student = Student.new("Baha", "Amayreh", "amayreh.1@osu.edu", "6412339999", "842963", "Computer Science")
+
+# Add 4 posts
+student.add_post("Student post 1")
+student.add_post("Student post 2")
+student.add_post("Student post 3")
+student.add_post("Student post 4")
+
+# Print the student information
+puts student.to_s
+
+# Print all posts
+student.posts.each do |post|
+  puts post.to_s
+end
