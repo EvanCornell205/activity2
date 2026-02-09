b2068690d6b72a599aad3255a5071a1ac7588f84
Evan Cornell <evan.m.cornell@gmail.com>
2026-02-09 15:27:32 -0500

    Post

---
+++ post.rb
@@ -0,0 +1,19 @@
+require 'date'
+
+class Post
+  attr_reader :id, :date, :content
+  
+  # Class variable to track the next post ID
+  @@next_id = 1
+  
+  def initialize(content)
+    @id = @@next_id
+    @@next_id += 1
+    @date = DateTime.now
+    @content = content
+  end
+  
+  def to_s
+    "Post ##{@id}, Date: #{@date}, #{@content}"
+  end
+end
